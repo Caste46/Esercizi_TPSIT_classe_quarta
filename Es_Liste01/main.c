745c2016cfc855c9969ee3ddc8a494c8eac15d43
Castellano Elia <91720492+Caste46@users.noreply.github.com>
2022-03-04 08:07:55 +0100

    Es_Liste01

--- Es_Liste01/main.c
+++ Es_Liste01/main.c
@@ -1,23 +1,49 @@
+/*
+Sia data una struttura dati dinamica di tipo Node semplicemente concatenata.
+Ogni nodo della Node contiene un numero intero come valore.  Si scriva la funzione che, dato un vettore di
+N numeri interi, restituisce la Node contenente gli N elementi del vettore;
+l’elemento di indice 0 va in testa alla Node, ecc.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct lista{
+typedef struct node{
     int num;
-    struct lista* next;
-}Lista;
+    struct node *next;
+} Node;
 
-int main()
-{
-    int n;
+Node *createNode(int val, Node *nodo){
+    Node *succ = (Node*)malloc(sizeof(Node));
 
-    printf("Inserisci il numero di valori: ");
-    scanf("%d", &n);
+    if(nodo != NULL){
+        nodo->next = succ;
+    }
+    succ->num = val;
+    succ->next = NULL;
+    return succ;
 
-    Lista* head = (Lista*) malloc(n * sizeof(Lista));
+}
 
+int main() {
 
+    int array[] = {1,2,3,4,5,6};
 
+    Node *head = NULL;
+    Node *cur = NULL;
+
+    for(int k = 0; k < 6; k++){
+        cur = createNode(array[k], cur);
+        if(head == NULL){
+            head = cur;
+        }
+    }
+
+    cur = head;
+    printf("ARREY: ");
+    while(cur != NULL){
+        printf("%d ", cur->num);
+        cur = cur->next;
+    }
     printf("\n");
-    system("PAUSE");
     return 0;
 }
